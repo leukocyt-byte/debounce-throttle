3f54790419b96f32971bd7a4d00efade20aecd28
Justyna <justynka.takk@gmail.com>
2021-03-28 20:43:14 +0200

    adding two buttons

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+let myDebouncedFn = debounce(function clickedFunction(event) {
+  console.log("click");
+  document.body.classList.toggle("negative");
+}, 500);
+
+document.getElementById("debounce-button").addEventListener("click", myDebouncedFn);
+
 const throttle = (func, limit) => {
   let inThrottle;
   return function () {
@@ -11,8 +33,8 @@ const throttle = (func, limit) => {
   };
 };
 
-document.querySelector("button").addEventListener("click", throttle(function myEfficientFn(event) {
-    console.log("click");
-    document.body.classList.toggle("negative");
-  }, 2000)
+document.getElementById("throttle-button").addEventListener("click", throttle(function myThrottledFn(event) {
+  console.log("click");
+  document.body.classList.toggle("negative");
+}, 2000)
 );
